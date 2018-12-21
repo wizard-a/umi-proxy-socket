00b0cabb87f1ee4ac52030ca2580b1e54ae1136e
jiechud <qiaojienet@163.com>
2018-12-21 10:03:48 +0800

    socket server

---
+++ server-socket/index.js
@@ -0,0 +1,14 @@
+const Koa = require('koa');
+const app = new Koa();
+const server = require('http').createServer(app.callback());
+const io = require('socket.io')(server);
+io.on('connection', (socket) => {
+    socket.emit('request', '发消息了！！！');
+    setInterval(() => {
+        socket.emit('request', '发消息了！！！');
+    }, 5000)
+    console.log('socket connection success');
+ });
+server.listen(3000, () => {
+    console.log('server start');
+});
